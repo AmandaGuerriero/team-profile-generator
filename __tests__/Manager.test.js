931c92b541c2ab898ebec67e53b6da705402622c
Amanda Guerriero <ag.codingclass@gmail.com>
2020-10-08 21:54:11 -0700

    Added Manger test and js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,11 @@
+const { expect } = require("@jest/globals");
 const Manager = require("../lib/Manager");
 
 test('manager name is set', () => {
-    const manager = new Manager('Michael');
+    const manager = new Manager('Manager', 'Michael', 100, 'email@email.com');
 
-    expect(manager.name).toBe('Michael');
+    expect(manager.role).toBe('Manager');
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
 })
